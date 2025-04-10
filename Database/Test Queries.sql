d4ccbc2b78201cb9a482e7c6c7a243939e841348
lukebryson <lukeb73@outlook.com>
2025-04-10 22:03:19 +0100

    Updated project write-up

--- Database/Test Queries.sql
+++ Database/Test Queries.sql
@@ -3,10 +3,22 @@ SELECT COUNT(*) AS total_devices
 FROM device_compliance;
 -- This query counts the total number of devices in the device_compliance table.
 
-SELECT COUNT(*) FROM device_compliance;
--- This query counts the total number of devices in the device_compliance table.
-
 SELECT deviceName, emailAddress, operatingSystem, complianceState
 FROM device_compliance
 WHERE complianceState != 'compliant';
 -- This query retrieves the device name, email address, operating system, and compliance state of devices that are not compliant.
+
+SELECT deviceName, emailAddress, freeStoragePct, totalStorageBytes, freeStorageBytes
+FROM device_compliance
+WHERE lowStorage = 1;
+--  Helps proactively flag devices at risk of performance issues.
+
+SELECT deviceName, osVersion, emailAddress
+FROM device_compliance
+WHERE osVersion != '10.0.22631.5039' AND operatingSystem = 'Windows';
+-- Check Devices Running Outdated OS Versions.
+
+SELECT deviceName, emailAddress, osVersion
+FROM device_compliance
+WHERE operatingSystem = 'iOS';
+-- Separates mobiles from Windows devices.
